de16553740b33eaeb11910e2ea5cc32052e59db3
fernandojr <contato@fernandojr.dev>
2020-01-24 14:48:14 -0300

    Remove config app and update serverless definition

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,6 @@
 from flask import Flask
-from .config import configure_app
 from .bureaucracy.controllers import bureaucracy
 
-app = Flask(
-    __name__,
-    instance_relative_config=False
-)
-
-configure_app(app)
+app = Flask(__name__)
 
 app.register_blueprint(bureaucracy, url_prefix="/forms")
